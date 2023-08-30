d8857a897a0f1e1ed972f411faaa7c5ffdc34d0b
payal <payal@LAPTOP-UM1PV0RU>
2023-08-30 21:05:12 +0530

    Add project files.

---
+++ CRUDOperationsonStudent/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDOperationsonStudent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            StudentOperation crud = new StudentOperation();
+            do
+            {
+                Console.WriteLine("1.Show Student Datails");
+                Console.WriteLine("2.Search Student by Id");
+                Console.WriteLine("3.Add Student");
+                Console.WriteLine("4.Update Record");
+                Console.WriteLine("5.Delete Record");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        List<Student> stlist = crud.ShowDetails();
+                        foreach(Student s in stlist)
+                        {
+                            Console.WriteLine($"{s.RollNo}\t{s.Name}");
+                            
+                        }
+                        break;
+                        case 2:
+                        Console.WriteLine("Enter Roll Number you want to search Record");
+                        int RollNo= Convert.ToInt32(Console.ReadLine());
+                        Student strec= crud.SearchByRollNo(RollNo);
+                        Console.WriteLine($"{strec.RollNo}\t{strec.Name}");
+                        break;
+                        case 3:
+                        Student stadd=new Student();
+                        Console.WriteLine("Enter Roll number");
+                        stadd.RollNo = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Name of Student");
+                        stadd.Name = Console.ReadLine();
+                        crud.AddStudent(stadd);
+                        break;
+                        case 4:
+                        Student stup=new Student();
+                        Console.WriteLine("Enter Roll Number of Student you want to Update");
+                        stup.RollNo= Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Name");
+                        stup.Name = Console.ReadLine();
+                        crud.UpdateRecord(stup);
+                        break;
+                        case 5:
+                       // Student stdel=new Student();
+                        Console.WriteLine("Enter Roll Number to Delete Record");
+                        int rollno= Convert.ToInt32(Console.ReadLine());
+                        crud.DeleteRecord(rollno);
+                        break;
+
+                }
+                Console.WriteLine("Enter 1 To Continue");
+                a = Convert.ToInt32(Console.ReadLine());
+            } while (a == 1);
+        }
+    }
+}
---
+++ CRUDOperationsonStudent/StudentOperation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDOperationsonStudent
+{
+    public class StudentOperation
+    {
+        List<Student> stdlist;
+        public StudentOperation() 
+        {
+            stdlist = new List<Student>()
+            {
+                new Student(){RollNo=1,Name="Payal"},
+                new Student(){RollNo=2,Name="Kajal"}
+            };
+        }
+
+        public List<Student> ShowDetails()
+        {
+            return stdlist;
+        }
+
+        public Student SearchByRollNo(int rollNo)
+        {
+            Student stR= new Student();
+            foreach (Student st in stdlist) 
+            {
+                if(st.RollNo == rollNo)
+                {
+                    stR = st;
+                    break;
+                }
+            }
+            return stR;
+        }
+
+        public void AddStudent(Student st)
+        {
+            stdlist.Add(st);
+        }
+
+        public void UpdateRecord(Student st)
+        {
+            foreach(Student st1 in stdlist)
+            {
+                if(st.RollNo==st.RollNo)
+                {
+                    st1.Name = st.Name;
+                    break;
+                }
+            }
+        }
+
+        public void DeleteRecord(int rollno)
+        {
+            foreach(Student st in stdlist)
+            {
+                if(st.RollNo==rollno)
+                {
+                    stdlist.Remove(st);
+                    break;
+                }
+            }
+
+        }
+
+    }
+}
